4fc89f1ec1d173a8720370e8118bf9405d3a2894
Rudraraiyani1902 <rudraraiyani1902@gmail.com>
2026-09-16 22:51:34 +0530

    MCP added

--- test.py
+++ test.py
@@ -1,15 +1,8 @@
-from tools.tavily_tool import tavily_search
-from tools.flight_tool import search_flights
-from backend import run_travel_agent
+import asyncio
+# from mcp_client_test import get_all_tools, tavily_mcp_search
+from mcp_client import get_all_tools
 
-# res=tavily_search("Best hotels in India")
-# print(res)
 
-user_input=input("Enter your travel query: ")
-response = run_travel_agent(
-    user_input=user_input,
-    thread_id="test_user"
-)
 
-print("\nFinal Response:\n")
-print(response["answer"])
+if __name__ == "__main__":
+    asyncio.run(get_all_tools())
